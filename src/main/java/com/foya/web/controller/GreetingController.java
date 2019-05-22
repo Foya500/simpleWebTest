685539ef52f6dbfcd41685aaaf02ec31ec959d23
Foya500 <49677962+Foya500@users.noreply.github.com>
2019-05-22 17:48:44 +0800

    init
    init

---
+++ src/main/java/com/foya/web/controller/GreetingController.java
@@ -0,0 +1,37 @@
+package com.foya.web.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.foya.web.bean.Greeting;
+
+@RestController
+public class GreetingController {
+	  private static final String template = "Hello, %s!";
+	    private final AtomicLong counter = new AtomicLong();
+
+	    @RequestMapping("/greeting")
+	    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	    	
+	        return new Greeting(counter.incrementAndGet(),
+	                            String.format(template, name));
+	    }
+	    
+	    static final String URL_EMPLOYEES = "http://10.1.1.252:8080/BCD_SB/index.jsp";
+	    @RequestMapping("/sendReq")
+	    public String sendReq(@RequestParam(value="path", defaultValue=URL_EMPLOYEES) String path) {
+	    	  RestTemplate restTemplate = new RestTemplate();
+	    	  
+	          // Send request with GET method and default Headers.
+	          String result = restTemplate.getForObject(path, String.class);
+	          
+	          System.out.println(result);
+	          
+	          return result;
+	    }
+	    
+	   
+}
---
+++ src/test/java/com/foya/web/WebServiceTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foya.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebServiceTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
