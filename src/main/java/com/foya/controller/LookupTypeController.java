f13a5870f5ca4933163af3e40b4144ee445ae00e
ardaliao <ardaliao@gmail.com>
2020-03-26 16:26:52 +0800

    change by arda for init

---
+++ src/main/java/com/foya/controller/LookupTypeController.java
@@ -0,0 +1,31 @@
+package com.foya.controller;
+
+import com.foya.model.LookupType;
+import com.foya.service.LookupTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("lookupType")
+public class LookupTypeController {
+
+		@Autowired
+		LookupTypeService lookupTypeService;
+
+	    @GetMapping("/findAll")
+	    public List<LookupType> findAll() {
+	       return lookupTypeService.findAll();
+	    }
+
+		@GetMapping("/findById")
+		public LookupType findById(@RequestParam(value="lookupType", defaultValue="") String lookupType) {
+			return lookupTypeService.findById(lookupType);
+		}
+
+
+}
--- src/test/java/com/foya/WebServiceTestApplicationTests.java
+++ src/test/java/com/foya/WebServiceTestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.foya.web;
+package com.foya;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
